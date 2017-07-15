8a94ea48a806e5d31da898c80c8cb49407196c27
Joey <burgee3d@yahoo.com>
2017-07-15 12:34:50 -0400

    Create IA-Contact.php

---
+++ IA-Contact.php
@@ -0,0 +1,25 @@
+<?php  
+	include 'modular.html';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>IA Contact</title>
+	<link rel="stylesheet" type="text/css" href="IAstyle.css">
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+	<center>
+		<h1><u>Information Academy</u></h1>
+		<h2>Contact Us:</h2>
+		<div class='content'>
+			
+		</div>
+	</center>
+	<footer>Information Academy 2017  |  Created By: Joey Burgee</footer>
+</body>
+</html>
