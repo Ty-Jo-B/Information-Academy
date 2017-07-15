ad522ffabd3da45ffa094ee2fab3711635dbae9e
Joey <burgee3d@yahoo.com>
2017-07-15 13:50:07 -0400

    Update IA-Contact.php

--- IA-Contact.php
+++ IA-Contact.php
@@ -9,7 +9,14 @@
 	<title>IA Contact</title>
 	<link rel="stylesheet" type="text/css" href="IAstyle.css">
 	<style type="text/css">
-
+		.topic {
+			margin-top: 1%;
+			color: rgb(255, 0, 0);
+		}
+		h3 {
+			margin-top: 1%;
+			color: rgb(255, 255, 255);
+		}
 	</style>
 </head>
 <body>
@@ -17,7 +24,15 @@
 		<h1><u>Information Academy</u></h1>
 		<h2>Contact Us:</h2>
 		<div class='content'>
-			
+			<h2 id='header'>Our Contact Info:</h2>
+			<h2 class='topic'><u>Email:</u></h2>
+			<h3>Work Email: company@example.com</h3>
+			<h3>Home Email: me@example.com</h3>
+			<h2 class='topic'><u>Phone:</u></h2>
+			<h3>Work Phone: (999) 999-9999</h3>
+			<h3>Home Phone: (999) 999-9999</h3>
+			<h2 class='topic'><u>Address:</u></h2>
+			<h3>Work Address: PO Box 999 Town, State</h3>
 		</div>
 	</center>
 	<footer>Information Academy 2017  |  Created By: Joey Burgee</footer>
